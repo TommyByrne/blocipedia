d314d6b2ade7d5b84ff548c9713cbd2a9b444743
tommybyrne <tbyrn687@gmail.com>
2015-02-22 13:08:55 -0600

    Initial Pundit Setup, with roles for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :wikis
+  after_initialize :init
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def admin?
+    role == 'admin'
+  end
+
+  def standard?
+    role == 'standard'
+  end
+
+  def premium?
+    role == 'premium'
+  end
+  def init
+    self.role ||= 'standard'
+  end
 end
